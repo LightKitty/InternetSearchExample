78475a3e290a683f146ce6b38b88df765df28745
yangguanghui <yangguanghui@bensen.com>
2019-10-09 20:39:32 +0800

    首次提交

---
+++ InternetSearchExample/FormSearch.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace InternetSearchExample
+{
+    partial class FormSearch
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBoxLinks = new System.Windows.Forms.ListBox();
+            this.listBoxImage = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "搜索网址：";
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(86, 6);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(605, 23);
+            this.textBoxUrl.TabIndex = 1;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(697, 6);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 2;
+            this.buttonSearch.Text = "搜索";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBoxLinks);
+            this.groupBox1.Location = new System.Drawing.Point(15, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 168);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "搜索结果1--网页中包含的超链接";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBoxImage);
+            this.groupBox2.Location = new System.Drawing.Point(399, 35);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(373, 168);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "搜索结果2--网页中包含的超链接";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBox1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 209);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(250, 186);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "图片预览";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.webBrowser1);
+            this.groupBox4.Location = new System.Drawing.Point(271, 209);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(501, 186);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "网页预览";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.richTextBox1);
+            this.groupBox5.Location = new System.Drawing.Point(12, 401);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(757, 148);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "网页对应源文件";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(745, 120);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 19);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(495, 164);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 158);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBoxLinks
+            // 
+            this.listBoxLinks.FormattingEnabled = true;
+            this.listBoxLinks.ItemHeight = 17;
+            this.listBoxLinks.Location = new System.Drawing.Point(6, 22);
+            this.listBoxLinks.Name = "listBoxLinks";
+            this.listBoxLinks.Size = new System.Drawing.Size(366, 140);
+            this.listBoxLinks.TabIndex = 0;
+            // 
+            // listBoxImage
+            // 
+            this.listBoxImage.FormattingEnabled = true;
+            this.listBoxImage.ItemHeight = 17;
+            this.listBoxImage.Location = new System.Drawing.Point(7, 22);
+            this.listBoxImage.Name = "listBoxImage";
+            this.listBoxImage.Size = new System.Drawing.Size(360, 140);
+            this.listBoxImage.TabIndex = 1;
+            this.listBoxImage.Click += new System.EventHandler(this.listBoxImage_Click);
+            // 
+            // FormSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormSearch";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Internet搜索";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBoxLinks;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBoxImage;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ InternetSearchExample/FormSearch.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+//添加的命名空间引用
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace InternetSearchExample
+{
+    public partial class FormSearch : Form
+    {
+        public FormSearch()
+        {
+            InitializeComponent();
+        }
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            listBoxLinks.Items.Clear();
+            listBoxImage.Items.Clear();
+            pictureBox1.Image = null;
+            webBrowser1.Url = new Uri("about:blank");
+            richTextBox1.Clear();
+            string urlString = textBoxUrl.Text.Trim();
+            if (urlString.StartsWith("http://") == false&& urlString.StartsWith("https://") == false)
+            {
+                urlString = "http://" + urlString;
+                textBoxUrl.Text = urlString;
+            }
+            string httpSource;
+            try
+            {
+                //设置鼠标形状为沙漏形状
+                Cursor.Current = Cursors.WaitCursor;
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8; // Encoding.GetEncoding("GB2312");
+                //获取包含网页源代码的字符串。
+                httpSource = webClient.DownloadString(textBoxUrl.Text);
+                richTextBox1.Text = httpSource;
+                webBrowser1.Url = new Uri(textBoxUrl.Text);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+                return;
+            }
+            finally
+            {
+                //设置鼠标形状为默认形状
+                Cursor.Current = Cursors.Default;
+            }
+            string regexHrefPattern = @"<a\s+href\s*=\s*""?([^"" >]+)""?>(.+)</a>";
+            Regex myRegex = new Regex(regexHrefPattern, RegexOptions.IgnoreCase);
+            Match myMatch = myRegex.Match(httpSource);
+            while (myMatch.Success == true)
+            {
+                listBoxLinks.Items.Add(myMatch.Groups[0].Value);
+                myMatch = myMatch.NextMatch();
+            }
+            string regexImgPattern =
+            @"<img[^>]+(src)\s*=\s*""?([^ "">]+)""?(?:[^>]+([^"">]+)""?)?";
+            myRegex = new Regex(regexImgPattern, RegexOptions.IgnoreCase);
+            myMatch = myRegex.Match(httpSource);
+            while (myMatch.Success == true)
+            {
+                listBoxImage.Items.Add(myMatch.Groups[2].Value);
+                myMatch = myMatch.NextMatch();
+            }
+        }
+        private void listBoxImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                pictureBox1.Image =
+                Image.FromStream(client.OpenRead(listBoxImage.SelectedItem.ToString()));
+            }
+            catch
+            {
+                pictureBox1.Image = null;
+            }
+        }
+    }
+}
